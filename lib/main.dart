e801f2ebd2bb8e2e78b4672cce80662b3ae99431
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2020-08-02 11:49:28 +0700

    Random dice through stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,26 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void updateLeft() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  void updateRight() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,15 +47,16 @@ class DicePage extends StatelessWidget {
             // Expanded promise to make thing expand its space.
             // Expanded have `flex` attribute, ratio of expanded space.
             Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10.0),
-                child: Image.asset('images/dice1.png'),
+              // remove
+              child: FlatButton(
+                onPressed: updateLeft,
+                child: Image.asset('images/dice$leftDiceNumber.png'),
               ),
             ),
             Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10.0),
-                child: Image.asset('images/dice2.png'),
+              child: FlatButton(
+                onPressed: updateRight,
+                child: Image.asset('images/dice$rightDiceNumber.png'),
               ),
             ),
           ],
